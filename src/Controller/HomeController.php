71a70d47e4d96cef5d74cf7c64f4e3d17bae2dab
Kain <mtheusmss@hotmail.com>
2024-06-11 03:10:25 -0300

    feature: making scripts in view template and create a new logic to save template when download canvas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ModelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ModelRepository $modelRepository): Response
     {
+
+        $user = $this->getUser();
+
+        $company_id = $user->getCompany()->getId();
+
+        $models = $modelRepository->findBy(['company' => $company_id]);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HelloWorld',
+            'models' => $models
         ]);
     }
 }
--- src/Controller/SavedTemplatesController.php
+++ src/Controller/SavedTemplatesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\TemplateRepository;
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,28 @@ use Symfony\Component\Routing\Attribute\Route;
 class SavedTemplatesController extends AbstractController
 {
     #[Route('/saved/templates', name: 'app_saved_templates')]
-    public function index(): Response
+    public function index(TemplateRepository $templateRepository): Response
     {
+
+        $user = $this->getUser();
+
+        $company_id = $user->getCompany()->getId();
+
+        $templateRepository->findBy(['user' => $company_id]);
+
         return $this->render('saved_templates/index.html.twig', [
             'controller_name' => 'Templates Salvos',
+            'templates' => $templateRepository
         ]);
+    }
+
+    public function store(Request $request, TemplateRepository $templateRepository): void
+    {
+        $this->store($request, $templateRepository);
+    }
+
+    public function destroy($id, TemplateRepository $templateRepository): void
+    {
+        $this->destroy($id, $templateRepository);
     }
 }
